2a3551242fa01b4f03fa04654ce216aca885ffc3
Yichao Yu <yyc1992@gmail.com>
2013-03-03 22:59:04 -0500

    [mossbauer] calibration convert to speed

--- mossbauer/calib.py
+++ mossbauer/calib.py
@@ -3,8 +3,17 @@
 import jlab
 from pylab import *
 
+wavelength = 632.8e-9
+
+def read_cal_file(fname):
+    f = jlab.load_pyfile(fname)
+    index, data = array([(d[0], d[2]) for d in f.data]).T
+    passes = int(f.elapsed_passes)
+    c_time = float(f.mossbauer_dwell_time.split()[0]) * 1e-3
+    return index, wavelength * data / 2 / passes / c_time
+
 def calib(iname, fig_name):
-    index, data = array([(d[0], d[2]) for d in jlab.load_pyfile(iname).data]).T
+    index, data = read_cal_file(iname)
     max_i = data[:len(data) // 2].argmax()
     v = data[max_i]
     for start in range(max_i + 1, len(data)):
@@ -13,11 +22,11 @@ def calib(iname, fig_name):
         v = data[start]
 
     fig1 = figure()
-    plot(index, data)
-    plot(start, data[start], 'o')
+    plot(index, data * 1000)
+    plot(start, data[start] * 1000, 'o')
     xlim(0, len(data))
     xlabel("Channel No.")
-    ylabel("Count per channel")
+    ylabel(r"Speed ($mm\cdot s^{-1}$)")
     grid()
     savefig(fig_name + '_raw.png')
     close()
@@ -29,17 +38,19 @@ def calib(iname, fig_name):
     fit = jlab.fitlin(index, data)
 
     fig2 = figure()
-    plot(index, data, label="Measure")
-    plot(index, fit.yfit, label="Fit")
+    plot(index, data * 1000, label="Measure")
+    plot(index, fit.yfit * 1000, label="Fit")
     legend(loc=2)
     xlim(index[0], index[-1])
     xmin, xmax = xlim()
     ymin, ymax = ylim()
-    b, a = jlab.a_pm_s(fit)
-    text(xmin * .9 + xmax * .1, ymin * .4 + ymax * .6,
-         "$y = a\cdot x + b$\n$a=%s$\n$b=%s$" % (a, b), fontsize=20)
+    b, a = jlab.a_pm_s([fit.a * 1000, fit.s * 1000], tex=True)
+    text(xmin * .95 + xmax * 0.05, ymin * .4 + ymax * .6,
+         "$y = a\cdot x + b$\n$a=%s mm\cdot s^{-1}$\n$b=%s mm\cdot s^{-1}$" %
+         (a, b),
+         fontsize=20)
     xlabel("Channel No.")
-    ylabel("Count per channel")
+    ylabel(r"Speed ($mm\cdot s^{-1}$)")
     grid()
     savefig(fig_name + '_fit.png')
     close()
