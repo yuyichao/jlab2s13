53848cd4f28e2398cfcf69ee2878b3f55f100dc2
Yichao Yu <yyc1992@gmail.com>
2013-04-14 12:18:31 -0400

    [pumping] add legend and axis label in plots

--- pumping/plot.py
+++ pumping/plot.py
@@ -5,15 +5,23 @@ from jlab import *
 
 def plot_data(iname, pname):
     data = load_pyfile(iname).data
+    lines = []
     ax1 = axes()
-    ax1.plot(data[0], data[1], 'b', linewidth=2.0)
+    lines += ax1.plot(data[0], data[1], 'b', linewidth=2.0,
+                      label="Intensity")
     if len(data) > 2:
         ax2 = twinx()
-        ax2.plot(data[0], data[2], 'r', linewidth=2.0)
+        lines += ax2.plot(data[0], data[2], 'r', linewidth=2.0,
+                          label="$B_z$ scan")
         ax2.set_zorder(ax1.get_zorder() - 1)
+        ax2.set_ylabel("$V_z/V$")
         ax1.patch.set_visible(False)
     grid()
     xlim([min(data[0]), max(data[0])])
+    xlabel("Scan Time/$s$")
+    ylabel("Light Intensity")
+    legend(lines, [line.get_label() for line in lines],
+           fancybox=True, loc='best')
     savefig(pname)
 
 if __name__ == '__main__':
