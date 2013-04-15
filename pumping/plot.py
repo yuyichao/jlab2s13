a190ca357c0146752b48114c00dc2fc8f1521766
Yichao Yu <yyc1992@gmail.com>
2013-04-15 03:05:50 -0400

    [pumping] update plot.

--- pumping/plot.py
+++ pumping/plot.py
@@ -5,21 +5,23 @@ from jlab import *
 
 def plot_data(iname, pname):
     data = load_pyfile(iname).data
-    lines = []
     ax1 = axes()
-    lines += ax1.plot(data[0], data[1], 'b', linewidth=2.0,
-                      label="Intensity")
+    lines = []
     if len(data) > 2:
         ax2 = twinx()
-        lines += ax2.plot(data[0], data[2], 'r', linewidth=2.0,
+        lines += ax1.plot(data[0], data[2], 'r', linewidth=2.0,
                           label="$B_z$ scan")
-        ax2.set_zorder(ax1.get_zorder() - 1)
-        ax2.set_ylabel("$V_z/V$")
-        ax1.patch.set_visible(False)
+        lines += ax2.plot(data[0], data[1], 'b', linewidth=2.0,
+                          label="Intensity")
+        ax2.set_ylabel("Light Intensity")
+        ax1.set_ylabel("$V_z/V$")
+    else:
+        lines += ax1.plot(data[0], data[1], 'b', linewidth=2.0,
+                          label="Intensity")
+        ax1.set_ylabel("Light Intensity")
     grid()
     xlim([min(data[0]), max(data[0])])
-    xlabel("Scan Time/$s$")
-    ylabel("Light Intensity")
+    ax1.set_xlabel("Scan Time/$s$")
     legend(lines, [line.get_label() for line in lines],
            fancybox=True, loc='best')
     savefig(pname)
