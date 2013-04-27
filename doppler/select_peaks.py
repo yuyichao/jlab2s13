319f21651acf7ebcf4616232106533d84c6ac7b7
Yichao Yu <yyc1992@gmail.com>
2013-04-27 19:28:38 -0400

    [doppler] select peaks

---
+++ doppler/select_peaks.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+from jlab import *
+
+def select_peaks(data_name, combine_name):
+    data = array(load_pyfile(data_name).data)
+    regions = load_pyfile(combine_name).interesting_regions
+    peaks = []
+    for start_i, end_i in regions:
+        plot(data[2][start_i:end_i])
+        plot(data[3][start_i:end_i])
+        plot((data[3] - data[2])[start_i:end_i])
+        title(data_name)
+        selector = RegionSelect()
+        for rect in selector.run():
+            peaks.append((rect.x1, rect.x2))
+    return peaks
+
+
+if __name__ == '__main__':
+    import sys
+    data_name, combine_name, oname = sys.argv[1:]
+    peaks_region = select_peaks(data_name, combine_name)
+    save_pyfile(Ret('peaks_region'), oname)
